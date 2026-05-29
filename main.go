f6460cce5d4c4641572b572bc9af00639a2bb8dd
sanenchen <srcenchen@gmail.com>
2026-05-30 00:09:02 +0800

    添加readme

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 	scheduler := pkg.NewScheduler()
 	scheduler.Start()
 	defer scheduler.Stop()
-	hsxkax a=11231;;; 
 	log.Println("Metrics collection scheduler started")
 
 	// Create API handler
